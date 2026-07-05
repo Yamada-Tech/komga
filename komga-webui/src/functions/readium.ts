1569dc8a16c6ce44fe03c4bdef5cccf285ea760c
Yamada-Tech <digitalcore660@hotmail.com>
2026-07-06 08:38:45 +0900

    fix: restore readium return shape

--- komga-webui/src/functions/readium.ts
+++ komga-webui/src/functions/readium.ts
@@ -33,25 +33,20 @@ function locationsToR2Location(location: Locations): R2Location {
   }
 }
 
-export function r2ProgressionToReadingPosition(bookId: string, progression?: R2Progression): ReadingPosition | undefined {
+export function r2ProgressionToReadingPosition(progression: R2Progression | undefined = undefined, bookId: string): ReadingPosition | undefined {
   if (!progression) return undefined
 
-  try {
-    return {
-      bookId,
-      created: progression.modified,
-      href: `${urls.originNoSlash}/api/v1/books/${bookId}/resource/${progression.locator.href}`,
-      type: progression.locator.type,
-      title: progression.locator.title,
-      locations: {
-        fragment: progression.locator.locations.fragment ? progression.locator.locations.fragment[0] : undefined,
-        position: progression.locator.locations.position,
-        progression: progression.locator.locations.progression,
-        totalProgression: progression.locator.locations.totalProgression,
-      },
-      text: progression.locator.text,
-    }
-  } catch (e) {
-    return undefined
+  return {
+    created: progression.modified,
+    href: `${urls.originNoSlash}/api/v1/books/${bookId}/resource/${progression.locator.href}`,
+    type: progression.locator.type,
+    title: progression.locator.title,
+    locations: {
+      fragment: progression.locator.locations.fragment ? progression.locator.locations.fragment[0] : undefined,
+      position: progression.locator.locations.position,
+      progression: progression.locator.locations.progression,
+      totalProgression: progression.locator.locations.totalProgression,
+    },
+    text: progression.locator.text,
   }
 }
