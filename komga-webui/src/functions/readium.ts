98a650b94cda24bb7154a8edc7450fcee11300a0
Yamada-Tech <digitalcore660@hotmail.com>
2026-07-06 08:44:06 +0900

    refactor: replace readium.ts with clean implementation

--- komga-webui/src/functions/readium.ts
+++ komga-webui/src/functions/readium.ts
@@ -1,37 +1,6 @@
-import {Locator, ReadingPosition} from '@d-i-t-a/reader'
-import {R2Location, R2Locator, R2Progression} from '@/types/readium'
-import {Locations} from '@d-i-t-a/reader/dist/types/model/Locator'
+import { ReadingPosition } from '@/types/custom'
+import { R2Progression } from '@/types/readium'
 import urls from '@/functions/urls'
-
-export function createR2Progression(locator: Locator): R2Progression {
-  return {
-    modified: new Date(),
-    device: {
-      name: 'Komga-webui',
-      id: 'unused',
-    },
-    locator: locatorToR2Locator(locator),
-  }
-}
-
-function locatorToR2Locator(locator: Locator): R2Locator {
-  return {
-    href: locator.href.startsWith(urls.originNoSlash) ? locator.href.replace(/(.*\/resource\/)/, '') : locator.href,
-    type: locator.type || 'application/octet-stream',
-    title: locator.title,
-    locations: locationsToR2Location(locator.locations),
-    text: locator.text,
-  }
-}
-
-function locationsToR2Location(location: Locations): R2Location {
-  return {
-    fragment: location.fragment ? [location.fragment] : [],
-    progression: location.progression,
-    position: location.position,
-    totalProgression: location.totalProgression,
-  }
-}
 
 export function r2ProgressionToReadingPosition(progression: R2Progression | undefined = undefined, bookId: string): ReadingPosition | undefined {
   if (!progression) return undefined
@@ -42,7 +11,7 @@ export function r2ProgressionToReadingPosition(progression: R2Progression | unde
     type: progression.locator.type,
     title: progression.locator.title,
     locations: {
-      fragment: progression.locator.locations.fragment ? progression.locator.locations.fragment[0] : undefined,
+      fragment: progression.locator.locations.fragment ? progression.locator.locations.fragment : undefined,
       position: progression.locator.locations.position,
       progression: progression.locator.locations.progression,
       totalProgression: progression.locator.locations.totalProgression,
